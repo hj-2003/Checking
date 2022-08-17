412cff6bbbfe5643f30cfd7580f87ca68fa9f66a
Harman <harman03jani@gmail.com>
2022-08-17 21:57:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./MyComponents/Header";
+import Footer from "./MyComponents/Footer";
+import Todos from "./MyComponents/Todos";
 
 function App() {
+  let todos =[
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to the market to get the job done"
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      desc: "You need to go to the market to get the job done 2"
+    },
+    {
+      sno: 3,
+      title: "Go to the ghat",
+      desc: "You need to go to the market to get the job done 3"
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title = "My Todos List" searchBar={true}/>
+      <Todos todos={todos}/>
+      <Footer/>
+    </>
   );
 }
 
